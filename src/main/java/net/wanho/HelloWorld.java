d29f19cb77b8f1b12029c5d8129017a4ef1e1f01
hucharyang <hucharyang@users.noreply.github.com>
2019-09-20 00:01:47 +0800

    Initial commit

---
+++ src/main/java/net/wanho/HelloWorld.java
@@ -0,0 +1,11 @@
+package net.wanho;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world11");
+        System.out.println("hello world22");
+        System.out.println("hello world33");
+        System.out.println("hello world44");
+        System.out.println("hello world55");
+    }
+}
